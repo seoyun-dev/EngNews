0fab7d8b36c9cf94902b7e7712680b2e14b2fcbc
seoyun <dhn04100@gmail.com>
2024-11-21 17:04:57 +0900

    FEAT app.py : try-translation

--- app.py
+++ app.py
@@ -12,14 +12,13 @@ app.secret_key = app_secret_key
 
 
 ##### GPT 설정
-llm = ChatOpenAI(temperature=0.0, model="gpt-3.5-turbo", openai_api_key=OPENAI_API_KEY)
+llm = ChatOpenAI(temperature=0.0, model="gpt-4o", openai_api_key=OPENAI_API_KEY)
 
 
 
+# 요약해보기 사용자별 메모리 초기화
+summarization_data_store = {}
 ##### 요약해보기 모델 (GPT)
-# 사용자별 메모리 초기화
-data_store = {}
-# app
 @app.route('/try-summarize', methods=['POST'])
 def try_summarize():
     user_id = request.headers.get('user')
@@ -32,9 +31,9 @@ def try_summarize():
         return jsonify({'error': 'message is required'}), 400
 
     # 사용자별 메모리 객체 생성 또는 가져오기
-    if user_id not in data_store:
-        data_store[user_id] = ConversationBufferMemory()
-    memory = data_store[user_id]
+    if user_id not in summarization_data_store:
+        summarization_data_store[user_id] = ConversationBufferMemory()
+    memory = summarization_data_store[user_id]
 
     # 대화 체인 초기화 (사용자별로 메모리와 연결된 대화 체인 생성)
     conversation_chain = ConversationChain(
@@ -58,10 +57,45 @@ def try_summarize():
 
 
 
+
+# 번역해보기 사용자별 메모리 초기화
+translation_data_store = {}
 ##### 번역해보기 모델 (GPT)
 @app.route('/try-translate', methods=['POST'])
 def try_translate():
-    return
+    user_id = request.headers.get('user')
+    user_message = request.json.get('message')
+    news_content = request.json.get('news_content', None)
+
+    if not user_id:
+        return jsonify({'error': 'user_id is required'}), 400
+    if not user_message:
+        return jsonify({'error': 'message is required'}), 400
+
+    # 사용자별 메모리 객체 생성 또는 가져오기
+    if user_id not in translation_data_store:
+        translation_data_store[user_id] = ConversationBufferMemory()
+    memory = translation_data_store[user_id]
+
+    # 대화 체인 초기화 (사용자별로 메모리와 연결된 대화 체인 생성)
+    conversation_chain = ConversationChain(
+        llm     = llm,
+        memory  = memory,
+        verbose = True  # 대화 히스토리 확인용
+    )
+
+    # 첫 번째 통신일 경우 뉴스 기사 내용 추가
+    if news_content and not memory.chat_memory.messages:
+        gpt_response= conversation_chain.predict(input=f"뉴스 문장: {news_content}\n \n 사용자가 뉴스 문장을 한글은 영어로, 영어는 한글로 번역한 문장: {user_message}\n\n사용자가 뉴스 문장을 번역한 문장에 대해 문법적, 내용적 등 다양한 기준을 고려하여 상세히 피드백을 해주세요. 사용자는 영어문장은 한글로 번역하고, 한글은 영어로 번역함으로써 영어 공부를 하고자 합니다. 사용자는 이를 통해 영어 공부를 하고자 하는 것이니 목적에 맞게 답변 해주세요. 뉴스 문장을 제대로 번역했는지 확인하세요! 그리고 당신은 한글로 피드백 해주세요!")
+    else:
+        # 사용자 메시지 처리
+        gpt_response = conversation_chain.predict(input=f"{user_message} \n 이는 이전 번역역습내용에 대한 추가적인 질문 혹은 사용자가다시 작성해본 한글은 영어로, 영어는 한글로 번역한 것입니다. 사용자는 이를 통해 영어 공부를 하고자 하는 것이니 목적에 맞게 답변 해주세요. 그리고 당신은 한글로 피드백 해주세요!")
+        
+    # 줄바꿈과 UTF-8 인코딩을 유지하여 JSON 형태로 반환
+    response_data = {
+        'gpt_answer': gpt_response
+    }
+    return Response(json.dumps(response_data, ensure_ascii=False, indent=2), content_type='application/json; charset=utf-8')
 
 
 
